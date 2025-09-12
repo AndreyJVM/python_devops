380b13d102da0b54309e54212503b16c46117c71
Andrey V. <andrey.vorobev.aqa@gmail.com>
2025-09-12 13:59:33 +0300

    Add README.md

---
+++ command_line_utilities/argparse_example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Утилита командной строки, использующая argparse
+"""
+
+import argparse
+def sail():
+    ship_name = 'Your ship'
+    print(f"{ship_name} is setting sail")
+    def list_ship():
+        ship = ['John B', 'Yankee Clipper', 'Peqoud']
